cf2222b47009e7d011a8c35344b165e292806544
Carolyn Hung <carolynhung@Carolyns-MacBook-Pro-2.local>
2017-03-04 20:57:59 -0800

    complete the steps

--- features/home_page.feature
+++ features/home_page.feature
@@ -7,21 +7,25 @@ Feature: homepage verification
     Given I login as a test user
     Then I should be able to login successfully
 
-  Scenario: search for a product
-    When I search a product from searchbar
+  Scenario: search for product
+    When I search eggs from searchbar
     And I should see a product I searched above on the page
 
   Scenario: Add a product into a cart
-    When I select a product from the page
-    And I add a product selected above into a cart
+    When I add a product from the page into a cart
     Then I should see a product added above in a cart
 
   Scenario: find a product from category and add a product into a cart
+    When I search fritos bean dip from searchbar
+    And I should see a product I searched above on the page
+    Then I add product searched above into a cart
+    And I should see above product in the cart
 
   Scenario: validate the cart for existing added-in product
     Given I login as a test user
-    Then I select a product from category menu
-    Then I add a product above into a cart
-    And I check the cart
-    And I should see above product is in the cart
+    Then I click on category menu
+    And I select Deli from the menu
+    And I should see products on the page
+    When I add a product from the page into a cart
+    And I should see above product in the cart
 
--- features/step_definitions/ui/home_page.rb
+++ features/step_definitions/ui/home_page.rb
@@ -14,20 +14,19 @@ Ane (/^I click on login button$/) do
   @home_page.click_button
 end
 
-Then (/^I should be able to login successfully$/) do
-  @home_page.find_login
-  @home_page.verify_login
-end
-
-When (/^I search a product from searchbar$/) do
+Then (/^I click on category menu$/) do
 
 end
 
-And (/^I should see a product I searched above on the page$/) do
+When (/^I search (.*) from searchbar$/) do |item|
 
 end
 
-When (/^I select a product from the page$/) do
+When (/^I add a product from the page$ into a cart/) do
+
+end
+
+Then (/^I add product searched above into a cart$/) do
 
 end
 
@@ -35,7 +34,11 @@ And (/^I add a product selected above into a cart$/) do
 
 end
 
-Then (/^I should see a product added above in a cart$/) do
+When (/^I add a product from the page into a cart$/) do
+
+end
+
+And (/^I select (.*) from the menu$/) do |category|
 
 end
 
@@ -43,14 +46,23 @@ Then (/^I select a product from category menu$/) do
 
 end
 
-Then (/^I add a product above into a cart$/) do
+Then (/^I should be able to login successfully$/) do
+  @home_page.find_login
+  @home_page.verify_login
+end
+
+And (/^I should see products on the page$/) do
 
 end
 
-And (/^I check the cart$/) do
+And (/^I should see above product in the cart$/) do
 
 end
 
-And (/^I should see above product is in the cart$/) do
+Then (/^I should see a product added above in a cart$/) do
+
+end
+
+And (/^I should see a product I searched above on the page$/) do
 
 end
